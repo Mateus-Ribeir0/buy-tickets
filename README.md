f3d3a1253189999c73cc7954a4f67d3e91cca713
Mateus Ribeiro <160142915+Mateus-Ribeir0@users.noreply.github.com>
2024-03-15 12:46:33 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# 🎟️ Compra de Ingressos 🎫
+
+## Sobre
+Desenvolvido como parte do curso de lógica de programação, este projeto permite aos usuários escolher a quantidade de ingressos desejada. O valor é automaticamente deduzido do total de ingressos disponíveis, exibindo mensagens de erro caso a ação não seja possível.
+
+### Funcionalidades
+- Escolha da quantidade de ingressos.
+- Atualização automática do total de ingressos disponíveis.
+- Mensagens de erro para ações inválidas.
+
+## Tecnologias
+![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/CSS-239120?&style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
---
+++ js/app.js
@@ -0,0 +1,85 @@
+/*function comprar() {
+    // Obter as informações do tipo do ingresso e a quantidade
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+    // Criar condicional para chamar as devidas funções
+    if (tipo.value == 'pista') {
+            comprarPista(qtd);
+    }
+    else if (tipo.value == 'superior') {
+            comprarSuperior(qtd);
+    }    
+    else if (tipo.value == 'inferior') {
+            comprarInferior(qtd);
+    }     
+    else {
+        alert('Tipo inválido');
+    }   
+}
+
+function comprarPista(qtd) {
+    // Encontrar o numero de ingressos da pista
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent); // qtd pista 100
+    // Decrecer valor total da pista da quantidade comprada
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else {
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else {
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+*/
+
+// Código otimizado
+
+function comprar() {
+    // Obter as informações do tipo do ingresso e a quantidade
+    let tipo = document.getElementById('tipo-ingresso');
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    // Verificar se o tipo de ingresso é válido
+
+    //>>>>>>>>>>>>>> Posso mudar o if para tipo.value != de "" para ser mais rápido
+    if (tipo.value === 'pista' || tipo.value === 'superior' || tipo.value === 'inferior') {
+        comprarIngresso(tipo.value, qtd);
+    } else {
+        alert('Tipo de ingresso inválido');
+    }
+}
+
+function comprarIngresso(tipo, qtd) {
+    // Encontrar o número de ingressos do tipo selecionado
+    let qtdIngresso = parseInt(document.getElementById(`qtd-${tipo}`).textContent);
+
+    // Decrementar valor total do tipo de ingresso da quantidade comprada
+    if (qtd > qtdIngresso) {
+        alert(`Quantidade indisponível para tipo ${tipo}`);
+    } else {
+        qtdIngresso -= qtd;
+        document.getElementById(`qtd-${tipo}`).textContent = qtdIngresso;
+        alert('Compra realizada com sucesso!');
+    }
+}
